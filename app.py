2e3339dfa884232b2f04f644ac05d43f059543c7
JasonWYZE <89949154+JasonWYZE@users.noreply.github.com>
2022-04-03 21:37:17 -0400

    update

--- app.py
+++ app.py
@@ -3,29 +3,23 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask, render_template, request
-from mbta_helper import get_nearest_station
-
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 
-
-@app.route('/result', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def nearestStation():
     if request.method == "POST":
-        return "hello world"
-        placeName = request.form["placeName"]
-        nearest_station, isAccessible = get_nearest_station(placeName)
+        placeName= request.form['placeName']
+
+
+        nearestStation, isAccessible = find_stop_near(placeName)
         return render_template("result.html", placeName = placeName, nearestStation = nearestStation, isAccessible = isAccessible)
     else:
         return render_template("index.html")
-
-
 
 
 if __name__ == "__main__":
